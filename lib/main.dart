3182c829b4498eb915f04608072abf5f8f87a722
Visnu-Ganth <visnuganth@mosaique.link>
2021-08-26 13:28:35 +0530

    Dev branch commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Feature1 Branch commit 6',
+      title: 'Dev branch new commit',
       theme: ThemeData(
         // This is the theme of your application.
         //
